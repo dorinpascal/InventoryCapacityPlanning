c8ec54ff74f45e34aedac73df40782cdb00ff50e
Dorin Pascal <dorin.pascal@siemensgamesa.com>
2024-10-23 19:35:27 +0200

    adding controller helper,  to handle exception mapping and logging in controller responses

---
+++ LEGO.Inventory.Capacity.Planning/Helpers/ControllerResponseMessageHelper.cs
@@ -0,0 +1,37 @@
+﻿using LEGO.Inventory.Capacity.Planning.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Authentication;
+
+namespace LEGO.Inventory.Capacity.Planning.Helpers;
+
+public static class ControllerResponseMessageHelper
+{
+    public static ObjectResult HandleException(Exception exception, ILogger logger)
+    {
+        var (statusCode, message, errors) = exception switch
+        {
+            ArgumentNullException notFoundException =>
+                (HttpStatusCode.NotFound, notFoundException.Message, null),
+            FormatException formatException =>
+                 (HttpStatusCode.BadRequest, formatException.Message, null),
+            ArgumentException argException =>
+                (HttpStatusCode.BadRequest, argException.Message, null),
+            AuthenticationException authenticationException =>
+                (HttpStatusCode.Unauthorized, authenticationException.Message, null),
+            UnauthorizedAccessException unauthorizedException =>
+                (HttpStatusCode.Forbidden, unauthorizedException.Message, null),
+            _ =>
+                (HttpStatusCode.ServiceUnavailable, "Service Unavailable", new List<string> { exception.Message })
+        };
+
+        logger.Log(statusCode == HttpStatusCode.ServiceUnavailable ? LogLevel.Error : LogLevel.Information, message);
+        var responseBody = new ErrorResponseBody
+        {
+            Message = message,
+            Errors = errors ?? []
+        };
+
+        return new ObjectResult(responseBody) { StatusCode = (int)statusCode };
+    }
+}
---
+++ LEGO.Inventory.Capacity.Planning/Models/ErrorResponseBody.cs
@@ -0,0 +1,7 @@
+﻿namespace LEGO.Inventory.Capacity.Planning.Models;
+
+public class ErrorResponseBody
+{
+    public string? Message { get; set; }
+    public List<string>? Errors { get; set; }
+}
